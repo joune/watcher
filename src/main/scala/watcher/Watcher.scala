6c42157c80ba373afd50157b267f64b1d79919f9
Arjun Panday <apanday@kyriba.com>
2014-02-10 11:44:12 +0100

    retab

--- src/main/scala/watcher/Watcher.scala
+++ src/main/scala/watcher/Watcher.scala
@@ -12,41 +12,41 @@ import collection.JavaConversions._
 
 object Watcher {
   def start(path: Path)(listener: ((String,Path)) => Unit) {
-		val watchService = FileSystems.getDefault.newWatchService
-		path.register(watchService, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE)
-		
-		var loop = true
-		
-		future {
-			while(loop) {
-				println("take...")
-				for {
-					key <- Try(watchService.take) 
-					ev <- key.pollEvents
-				} {
+    val watchService = FileSystems.getDefault.newWatchService
+    path.register(watchService, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE)
+    
+    var loop = true
+    
+    future {
+      while(loop) {
+        println("take...")
+        for {
+          key <- Try(watchService.take) 
+          ev <- key.pollEvents
+        } {
           println(ev.kind.name)
-					listener((ev.kind.name, ev.context.asInstanceOf[Path]))
-					if (!key.reset) loop = false
-				}
-			}
-		}
-	}
+          listener((ev.kind.name, ev.context.asInstanceOf[Path]))
+          if (!key.reset) loop = false
+        }
+      }
+    }
+  }
 }
 
 class WatcherActor extends Actor {
-	import Watcher._
-	def receive = {
-		case path: String =>
-		 	val to = sender
-			start(Paths.get(path)) { case evt =>
-				println(s"$evt => $to")
-				to ! evt
-			}
-	}
+  import Watcher._
+  def receive = {
+    case path: String =>
+      val to = sender
+      start(Paths.get(path)) { case evt =>
+        println(s"$evt => $to")
+        to ! evt
+      }
+  }
 }
 
 object WatcherMain extends App {
-	import Watcher._
-	start(Paths.get(args(0)))(println)
-	System.in.read
+  import Watcher._
+  start(Paths.get(args(0)))(println)
+  System.in.read
 }
--- src/test/scala/watcher/WatcherSpec.scala
+++ src/test/scala/watcher/WatcherSpec.scala
@@ -21,43 +21,43 @@ class WatcherSpec extends TestKit(ActorSystem("WatcherSpec"))
 
   override def afterAll(): Unit = system.shutdown
 
-	val dir = Files.createTempDirectory(Paths.get("target"), "test-")
+  val dir = Files.createTempDirectory(Paths.get("target"), "test-")
 
   test("outside akka") {
-		import Watcher._
-		var events = List[(String,Path)]()
-		val c = Promise[String]()
-		val d = Promise[String]()
-		start(dir) { evt =>
-			events = evt :: events
-			if (evt._1 == "ENTRY_CREATE") c success evt._1
-			if (evt._1 == "ENTRY_DELETE") d success evt._1
-		}
-		val f = Files.createTempFile(dir, "file-", "")
-		var expected = List(("ENTRY_CREATE",f.getFileName))
-		//??? expected = List(("ENTRY_MODIFY",f.getFileName)) ::: expected
-		Thread.sleep(100)
-		assert(expected === events)
-		assert(Await.result(c.future, 1 seconds) == "ENTRY_CREATE")
-		Files.write(f, "modif".getBytes)
-		expected = List(("ENTRY_MODIFY",f.getFileName)) ::: expected
-		Thread.sleep(100)
-		assert(expected === events)
-		Files.delete(f)
-		expected = List(("ENTRY_DELETE",f.getFileName)) ::: expected
-		Thread.sleep(100)
-		assert(expected === events)
-		assert(Await.result(d.future, 1 seconds) == "ENTRY_DELETE")
-	}
+    import Watcher._
+    var events = List[(String,Path)]()
+    val c = Promise[String]()
+    val d = Promise[String]()
+    start(dir) { evt =>
+      events = evt :: events
+      if (evt._1 == "ENTRY_CREATE") c success evt._1
+      if (evt._1 == "ENTRY_DELETE") d success evt._1
+    }
+    val f = Files.createTempFile(dir, "file-", "")
+    var expected = List(("ENTRY_CREATE",f.getFileName))
+    expected = List(("ENTRY_MODIFY",f.getFileName)) ::: expected
+    Thread.sleep(100)
+    assert(expected === events)
+    assert(Await.result(c.future, 1 seconds) == "ENTRY_CREATE")
+    Files.write(f, "modif".getBytes)
+    expected = List(("ENTRY_MODIFY",f.getFileName)) ::: expected
+    Thread.sleep(100)
+    assert(expected === events)
+    Files.delete(f)
+    expected = List(("ENTRY_DELETE",f.getFileName)) ::: expected
+    Thread.sleep(100)
+    assert(expected === events)
+    assert(Await.result(d.future, 1 seconds) == "ENTRY_DELETE")
+  }
 
   test("inside akka") {
     system.actorOf(Props(new WatcherActor)) ! dir.toString
-		val f = Files.createTempFile(dir, "file-", "")
-		expectMsg(("ENTRY_CREATE",f.getFileName))
-		Files.write(f, "modif".getBytes)
-		expectMsg(("ENTRY_MODIFY",f.getFileName))
-		Files.delete(f)
-		expectMsg(("ENTRY_DELETE",f.getFileName))
+    val f = Files.createTempFile(dir, "file-", "")
+    expectMsg(("ENTRY_CREATE",f.getFileName))
+    Files.write(f, "modif".getBytes)
+    expectMsg(("ENTRY_MODIFY",f.getFileName))
+    Files.delete(f)
+    expectMsg(("ENTRY_DELETE",f.getFileName))
 
   }
 
